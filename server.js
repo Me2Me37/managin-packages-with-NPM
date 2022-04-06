d516f26ed8dcb10f8e3604040f641e068c19e774
Yusuf Ashki <yousefashki1@gmail.com>
2022-04-06 14:19:28 +0300

    Add a Hello Express

--- server.js
+++ server.js
@@ -55,6 +55,10 @@ if (!process.env.DISABLE_XORIGIN) {
 //   }  
 // })
 
+app.get("/", function(req, res) {
+  res.send("Hello Express")
+});
+
 //Listen on port set in environment variable or default to 3000
 var port = process.env.PORT || 3000;
 const listener = bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function () {
