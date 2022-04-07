846889a5c52058c8bda74819b5af9231f624a087
Yusuf Ashki <yousefashki1@gmail.com>
2022-04-07 17:30:21 +0300

    Add dotenv package and connect it to myApp

--- myApp.js
+++ myApp.js
@@ -1,14 +1,34 @@
+const { config } = require('dotenv');
 var express = require('express');
 var app = express();
 var bGround = require('fcc-express-bground');
+require("dotenv").config()
 
 bGround.log("Hello World");
 console.log("Hello World");
 
-app.get("/json", function(req, res) {
-    res.json({"message": "Hello json"})
-});
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"))
 
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html")
+  });
+
+// app.get("/json", function(req, res) {
+//     res.json({"message": "Hello json"});
+// });
+
+app.get("/json", function(req, res) {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json(
+            { "message": "HELLO JSON"}
+        )
+    } else {
+        res.json(
+            { "message": "Hello json"}
+        )
+    }
+});
 
 
 
